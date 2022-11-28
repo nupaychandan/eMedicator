f68391b867a5371e52bd5fb31e778323de91eb97
Sonam23verma <vermsonam945@gmail.com>
2022-11-28 11:32:04 +0530

    add faq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Router, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -10,8 +11,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
-
 import { HttpClientModule} from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+import { FaqComponent } from './faq/faq.component';
 
 
 @NgModule({
@@ -24,13 +26,18 @@ import { HttpClientModule} from '@angular/common/http';
     DashboardComponent,
     RegistrationComponent,
     HomepageComponent,
-    
+    FaqComponent,
+
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-    
+    HttpClientModule,
+    RouterModule,
+    FormsModule
+
   ],
   providers: [],
   //bootstrap: [LoginComponent]
